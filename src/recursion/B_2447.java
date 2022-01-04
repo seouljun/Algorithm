f61dd776b6d8633655b957d38fb2ae91b41a5004
JKK <kkj5270@naver.com>
2022-01-04 15:52:04 +0900

    별찍기 어려웠음.. 나중에 다시 풀어봐야함
    힌트 : 큰걸 작은걸로 쪼갠다

--- src/recursion/B_2447.java
+++ src/recursion/B_2447.java
@@ -3,26 +3,63 @@ package recursion;
 import java.io.*;
 
 public class B_2447 {
+    static String[][] arr;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        printStar(n);
+
+        //별 찍을 배열 생성
+        arr = new String[n][n];
+
+        makeStar(n, 0, 0, false);
+        printStar(arr,n, bw);
+
         bw.flush();
         bw.close();
         br.close();
     }
 
-    static void printStar(int value) {
-        for (int i = 0; i < value; i++) {
-            for (int j = 0; j < value; j++) {
-                if(i%3== 1 && j%3==1) {
-                    System.out.print(" ");
+    static void makeStar(int value, int x, int y, boolean isBlank) {
+        //공백
+        if (isBlank) {
+            for (int i = x; i < x + value; i++) {
+                for (int j = y; j < y + value; j++) {
+                    arr[i][j] = " ";
                 }
-                else
-                    System.out.print("*");
             }
-            System.out.println();
+            return;
         }
+
+
+        //별
+        if (value == 1) {
+            arr[x][y] = "*";
+            return;
+        }
+
+        int size = value / 3;
+        int count = 0;
+        for (int i = x; i < x+value; i+=size) {
+            for (int j = y; j < y+value; j+=size) {
+                count++;
+                if (count == 5) {
+                    makeStar(size, i, j, true);
+                } else {
+                    makeStar(size, i, j, false);
+                }
+            }
+        }
+    }
+    static void printStar(String[][] arr, int value, BufferedWriter bw) throws IOException {
+        for(int i = 0 ; i < value ; i++){
+            for(int j = 0 ; j < value ; j++){
+                bw.write(arr[i][j]);
+            }
+            bw.write("\n");
+        }
+
     }
 }
