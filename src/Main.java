b491a0220b9ccb04a28738a69b80a3bb7f3cb621
JKK <kkj5270@naver.com>
2022-01-26 20:51:46 +0900

    dp 가장 긴 바이토닉 부분 수열

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,70 @@
 import java.util.Scanner;
 
 public class Main {
-    static long[][] d;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        d = new long[n+1][10];
-        long count = 0;
-        for(int i = 0 ; i <=9 ; i++){
-            d[1][i] = 1L;
+
+    static Integer[] r_dp;
+    static Integer[] l_dp;
+    static int[] seq;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        r_dp = new Integer[N];	// LIS dp
+        l_dp = new Integer[N];	// LDS dp
+        seq = new int[N];
+
+
+        for (int i = 0; i < N; i++) {
+            seq[i] = in.nextInt();
         }
-        for(int i = 1; i <=9 ; i++){
-            count += doCal(n,i);
+
+        for (int i = 0; i < N; i++) {
+            LIS(i);
+            LDS(i);
         }
-        System.out.println(count % 1000000000);
+
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(r_dp[i] + l_dp[i], max);
+        }
+        System.out.println(max - 1);
+
     }
 
-    private static long doCal(int n, int value) {
-        if(n == 1){
-            return d[n][value];
-        }
-        if(d[n][value] != 0) return d[n][value];
+    static int LIS(int N) {
 
-        if(value == 0){
-            d[n][value] = doCal(n-1,1);
+        // 만약 탐색하지 않던 위치의 경우
+        if (r_dp[N] == null) {
+            r_dp[N] = 1; // 1로 초기화
+
+            // N 이전의 노드들을 탐색
+            for (int i = N - 1; i >= 0; i--) {
+                // 이전의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
+                if (seq[i] < seq[N]) {
+                    r_dp[N] = Math.max(r_dp[N], LIS(i) + 1);
+                }
+            }
         }
-        else if(value == 9){
-            d[n][value] = doCal(n-1,8);
+        return r_dp[N];
+    }
+
+    static int LDS(int N) {
+
+        // 만약 탐색하지 않던 위치의 경우
+        if (l_dp[N] == null) {
+            l_dp[N] = 1; // 1로 초기화
+
+            // N 이후의 노드들을 탐색
+            for (int i = N + 1; i < l_dp.length; i++) {
+                // 이후의 노드 중 seq[N]의 값보다 작은 걸 발견했을 경우
+                if (seq[i] < seq[N]) {
+                    l_dp[N] = Math.max(l_dp[N], LDS(i) + 1);
+                }
+            }
         }
-        else {
-            d[n][value] = doCal(n-1,value-1) + doCal(n-1,value+1);
-        }
-        return d[n][value] % 1000000000;
+        return l_dp[N];
     }
 }
