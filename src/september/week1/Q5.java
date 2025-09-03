f5aac1a02e8df0dacb04020b85ea6355d7f17754
seouljun <seouljun@innerwave.co.kr>
2025-09-03 11:48:59 +0900

    정렬 2

---
+++ src/september/week1/Q5.java
@@ -0,0 +1,52 @@
+package september.week1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/*
+문제
+알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+
+길이가 짧은 것부터
+길이가 같으면 사전 순으로
+단, 중복된 단어는 하나만 남기고 제거해야 한다.
+
+입력
+첫째 줄에 단어의 개수 N이 주어진다. (1 ≤ N ≤ 20,000) 둘째 줄부터 N개의 줄에 걸쳐 알파벳 소문자로 이루어진 단어가 한 줄에 하나씩 주어진다. 주어지는 문자열의 길이는 50을 넘지 않는다.
+
+출력
+조건에 따라 정렬하여 단어들을 출력한다.
+ */
+public class Q5
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int cnt = Integer.parseInt(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < cnt; i++)
+        {
+            String input = br.readLine();
+            set.add(input);
+        }
+        List<String> returnList = set.stream().sorted(Comparator.comparing(String::length).thenComparing(v -> v))
+            .collect((Collectors.toList()));
+        for (String s : returnList)
+        {
+            bw.write(s + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
